3425d5abbea4bac94218fd859f0460613a5263ec
ConnorDuncan <s-duncanc@bsd405.org>
2020-11-13 15:17:44 -0800

    Create makesticky.js

---
+++ makesticky.js
@@ -0,0 +1,14 @@
+
+window.onscroll = function() {makeSticky()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function makeSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
