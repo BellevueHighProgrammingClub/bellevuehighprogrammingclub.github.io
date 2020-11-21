1d82ff98fed59f35ad9760bbbe0f456c69ea97d4
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2020-11-20 16:27:21 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/*
 window.onscroll = function() {makeSticky()};
 
 var navbar = document.getElementById("topnav");
@@ -10,5 +10,25 @@ function makeSticky() {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
+  }
+}
+*/
+
+window.onscroll = function() {makeSticky()};
+
+var navbar = document.getElementById("topnav");
+var navbarHeight = 60 + navbar.offsetHeight + "px";
+var content = document.getElementById("content");
+
+var sticky = navbar.offsetTop;
+
+function makeSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    content.style.marginTop = navbarHeight;
+  } 
+  else {
+    navbar.classList.remove("sticky");
+    content.style.marginTop = "0px";
   }
 }
