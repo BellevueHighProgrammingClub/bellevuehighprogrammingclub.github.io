fa2256f6f7b9e9f465694042543e736b9a0b72be
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2020-12-04 15:19:35 -0800

    Added code to make the dropdown work on scroll

--- app.js
+++ app.js
@@ -13,22 +13,26 @@ function makeSticky() {
   }
 }
 */
-
 window.onscroll = function() {makeSticky()};
 
 var navbar = document.getElementById("topnav");
 var navbarHeight = 60 + navbar.offsetHeight + "px";
+var navbarHeightForDropdown = navbar.offsetHeight + "px";
 var content = document.getElementById("content");
+var dropdown = document.getElementById("dropdown-content");
 
 var sticky = navbar.offsetTop;
 
 function makeSticky() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
+    dropdown.classList.add("dropdown-sticky");
     content.style.marginTop = navbarHeight;
   } 
   else {
     navbar.classList.remove("sticky");
     content.style.marginTop = "0px";
+    dropdown.classList.remove("dropdown-sticky");
+
   }
 }
