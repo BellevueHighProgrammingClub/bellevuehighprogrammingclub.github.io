84428bd9dd4bd88feb58a86fb98c93ee0e4a1ee1
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2020-12-18 15:43:03 -0800

    Update app.js

--- javascriptFiles/app.js
+++ javascriptFiles/app.js
@@ -1,38 +1,26 @@
-/*
-window.onscroll = function() {makeSticky()};
-
-var navbar = document.getElementById("topnav");
-
-var sticky = navbar.offsetTop;
-
-function makeSticky() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-*/
 window.onscroll = function() {makeSticky()};
 
 var navbar = document.getElementById("topnav");
 var navbarHeight = 60 + navbar.offsetHeight + "px";
 var navbarHeightForDropdown = navbar.offsetHeight + "px";
 var content = document.getElementById("content");
-var dropdown = document.getElementById("dropdown-content");
+var dropdownmembers = document.getElementById("dropdown-content");
+var dropdownevents = document.getElementById("dropdown-content-events");
 
 var sticky = navbar.offsetTop;
 
 function makeSticky() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
-    dropdown.classList.add("dropdown-sticky");
+    dropdownmembers.classList.add("dropdown-sticky");
+    dropdownevents.classList.add("dropdown-sticky");
     content.style.marginTop = navbarHeight;
   }
   else {
     navbar.classList.remove("sticky");
     content.style.marginTop = "0px";
-    dropdown.classList.remove("dropdown-sticky");
+    dropdownmembers.classList.remove("dropdown-sticky");
+    dropdownevents.classList.remove("dropdown-sticky");
 
   }
 }
